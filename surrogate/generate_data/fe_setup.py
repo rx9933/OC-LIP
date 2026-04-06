e6be45b34fabafff255a68e9689d2f1f70042a30
fredkhouri <fmk354@my.utexas.edu>
2026-04-06 09:36:50 -0400

    Update fe_setup.py

--- surrogate/generate_data/fe_setup.py
+++ surrogate/generate_data/fe_setup.py
@@ -95,3 +95,43 @@ def setup_true_initial_condition(Vh):
         element=Vh.ufl_element()
     )
     return dl.interpolate(ic_expr, Vh).vector()
+
+def setup_fe_spaces_buildings(mesh_file='/home/fredkhouri/hippylib/applications/ad_diff/ad_20.xml'):
+    """
+    Set up FE spaces using the hIPPYlib ad_20 mesh with buildings.
+    """
+    mesh = dl.refine(dl.Mesh(mesh_file))
+    Vh = dl.FunctionSpace(mesh, "Lagrange", 1)
+    
+    # Lid-driven cavity wind (same as hIPPYlib tutorial)
+    Xh = dl.VectorFunctionSpace(mesh, 'Lagrange', 2)
+    Wh = dl.FunctionSpace(mesh, 'Lagrange', 1)
+    mixed_element = ufl.MixedElement([Xh.ufl_element(), Wh.ufl_element()])
+    XW = dl.FunctionSpace(mesh, mixed_element)
+
+    Re = dl.Constant(1e2)
+    g = dl.Expression(('0.0','(x[0] < 1e-14) - (x[0] > 1 - 1e-14)'), degree=1)
+    
+    bc1 = dl.DirichletBC(XW.sub(0), g, v_boundary)
+    bc2 = dl.DirichletBC(XW.sub(1), dl.Constant(0), q_boundary, 'pointwise')
+    bcs = [bc1, bc2]
+
+    vq = dl.Function(XW)
+    (v, q) = ufl.split(vq)
+    (v_test, q_test) = dl.TestFunctions(XW)
+
+    def strain(v):
+        return ufl.sym(ufl.grad(v))
+
+    F = ((2./Re)*ufl.inner(strain(v), strain(v_test))
+         + ufl.inner(ufl.nabla_grad(v)*v, v_test)
+         - q*ufl.div(v_test)
+         + ufl.div(v)*q_test) * ufl.dx
+
+    dl.solve(F == 0, vq, bcs,
+             solver_parameters={"newton_solver":
+                                 {"relative_tolerance": 1e-4,
+                                  "maximum_iterations": 100}})
+
+    wind_velocity = dl.project(v, Xh)
+    return mesh, Vh, wind_velocity
