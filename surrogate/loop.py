b9eaeca4c988cb9b41f82c95b0835fa0012b504d
fredkhouri <fmk354@my.utexas.edu>
2026-04-08 21:47:02 -0500

    Update loop.py

--- surrogate/loop.py
+++ surrogate/loop.py
@@ -2,11 +2,11 @@ import numpy as np
 import os
 import subprocess
 import training_loop as tl
-
-max_n_train = 1600
-n_test = 100
+max_n_data = 1948
+n_test = 200
+trains = [1600]
 max_n_data = 945
-trains = [800]# * (2**i) for i in range(int(np.log2(max_n_train/50)) + 1)]
+# * (2**i) for i in range(int(np.log2(max_n_train/50)) + 1)]
 for n_train in trains:
     if n_train>0:
         
@@ -16,15 +16,14 @@ for n_train in trains:
             f'--n_train={n_train}',
             f'--n_test={n_test}',
             f'--n_data={max_n_data}',# 13000
-            '--rQ=202', # currently unused, reduced data dimension
-            '--dQ=202', # reduced velocity + position dimension
-            '--dM=14', # parameter dimension: 4K+2 , path coefficients
-            '--rM=14' # currently unused
+            '--rQ=22', # 2 position + 20 POD modes
+            '--dQ=22', # 2 position + 20 POD modes
+            '--dM=14', # parameter dimension: 4K+2 , path coefficients (after IC fix)
+            '--rM=12' # base NN output: 12 Fourier amplitudes (x_bar, y_bar computed by PathNetwork)
         ]
         
         print("Executing:", ' '.join(cmd))
     
-
     try:
         subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError as e:
